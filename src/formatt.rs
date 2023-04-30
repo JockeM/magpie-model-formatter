78cbd00bebc9b7a23697ced0d900a23846b4cabb
Joakim Malmberg <10110847+JockeM@users.noreply.github.com>
2023-04-30 21:22:17 +0200

    Change so comments and empty lines are not removed

--- src/formatt.rs
+++ src/formatt.rs
@@ -5,36 +5,54 @@ pub fn format_model_file(contents: String) -> Result<String, Box<dyn std::error:
     Ok(formatted_output)
 }
 
-// TODO: better typed, keep comments etc
-fn parse_file_contents(contents: &str) -> Vec<Vec<&str>> {
+#[derive(Clone, Debug, PartialEq)]
+enum Line {
+    Empty,
+    Comment(String),
+    Model(Vec<String>),
+}
+
+fn parse_file_contents(contents: &str) -> Vec<Line> {
     contents
         .lines()
         .map(|line| line.trim())
-        .filter_map(|line| {
+        .map(|line| {
+            if line.is_empty() {
+                return Line::Empty;
+            }
             if line.starts_with('#') {
-                return None;
+                return Line::Comment(line.to_string());
             }
 
             let mut parts = line
                 .split("  ")
-                .map(|part| part.trim())
+                .map(|part| part.trim().to_string())
                 .filter(|part| !part.is_empty())
-                .collect::<Vec<&str>>();
+                .collect::<Vec<String>>();
 
             if parts.len() > 1 && !parts[1].starts_with('[') {
-                parts.insert(1, "");
+                parts.insert(1, "".to_string());
             }
 
-            Some(parts)
+            Line::Model(parts)
         })
         .collect()
 }
 
-fn calculate_max_widths(lines: &[Vec<&str>]) -> Vec<usize> {
-    let num_columns = lines.iter().map(Vec::len).max().unwrap_or(0);
+fn calculate_max_widths(lines: &[Line]) -> Vec<usize> {
+    let parts = lines
+        .iter()
+        .filter_map(|line| match line {
+            Line::Empty => None,
+            Line::Comment(_) => None,
+            Line::Model(parts) => Some(parts.clone()),
+        })
+        .collect::<Vec<_>>();
+
+    let num_columns = parts.iter().map(Vec::len).max().unwrap_or(0);
     (0..num_columns)
         .map(|column| {
-            lines
+            parts
                 .iter()
                 .flat_map(|parts| parts.get(column))
                 .map(|part| part.len())
@@ -44,20 +62,26 @@ fn calculate_max_widths(lines: &[Vec<&str>]) -> Vec<usize> {
         .collect()
 }
 
-fn generate_aligned_output(lines: &[Vec<&str>], max_widths: &[usize]) -> String {
+fn generate_aligned_output(lines: &[Line], max_widths: &[usize]) -> String {
     let mut output = String::new();
 
-    for parts in lines {
-        for (i, part) in parts.iter().enumerate() {
-            output.push_str(part);
+    for line in lines {
+        match line {
+            Line::Empty => (),
+            Line::Comment(a) => output.push_str(a),
+            Line::Model(parts) => {
+                for (i, part) in parts.iter().enumerate() {
+                    output.push_str(part);
 
-            if i != parts.len() - 1 {
-                let max_width = max_widths.get(i).unwrap_or(&0);
-                let padding = " ".repeat(max_width.saturating_sub(part.len()));
-                output.push_str(&padding);
-                output.push_str("  ");
+                    if i != parts.len() - 1 {
+                        let max_width = max_widths.get(i).unwrap_or(&0);
+                        let padding = " ".repeat(max_width.saturating_sub(part.len()));
+                        output.push_str(&padding);
+                        output.push_str("  ");
+                    }
+                }
             }
-        }
+        };
 
         output.push('\n');
     }
@@ -70,52 +94,33 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_parse_file_contents() {
-        let contents = r#"# This is a comment
-            a                 b  ->  c
-            hello world  [x]  b  ->  c"#;
-        let lines = parse_file_contents(contents);
-        assert_eq!(lines.len(), 2);
-        assert_eq!(lines[0].len(), 5);
-        assert_eq!(lines[1].len(), 5);
-        assert_eq!(lines[0][0], "a");
-        assert_eq!(lines[0][1], "");
-        assert_eq!(lines[0][2], "b");
-        assert_eq!(lines[0][3], "->");
-        assert_eq!(lines[0][4], "c");
-        assert_eq!(lines[1][0], "hello world");
-        assert_eq!(lines[1][1], "[x]");
-        assert_eq!(lines[1][2], "b");
-        assert_eq!(lines[1][3], "->");
-        assert_eq!(lines[1][4], "c");
-    }
-
-    #[test]
-    fn test_calculate_max_widths() {
-        let lines = vec![
-            vec!["a", "bb", "ccc"],
-            vec!["dddd", "eee", "f"],
-            vec!["ggggg", "hh", "i"],
-        ];
-        let max_widths = calculate_max_widths(&lines);
-        assert_eq!(max_widths.len(), 3);
-        assert_eq!(max_widths[0], 5);
-        assert_eq!(max_widths[1], 3);
-        assert_eq!(max_widths[2], 3);
-    }
-
-    #[test]
     fn test_generate_aligned_output() {
         let lines = vec![
-            vec!["a", "bb", "ccc"],
-            vec!["dddd", "eee", "f"],
-            vec!["ggggg", "hh", "i"],
+            Line::Comment("# This is a comment".to_string()),
+            Line::Empty,
+            Line::Model(vec![
+                "Model A".to_string(),
+                "[some description]".to_string(),
+                "->".to_string(),
+                "false".to_string(),
+            ]),
+            Line::Model(vec![
+                "Model B".to_string(),
+                "12345678".to_string(),
+                "->".to_string(),
+                "true".to_string(),
+            ]),
         ];
-        let max_widths = vec![5, 3, 3];
-        let output = generate_aligned_output(&lines, &max_widths);
-        let expected_output = "a      bb   ccc\n\
-                               dddd   eee  f\n\
-                               ggggg  hh   i\n";
-        assert_eq!(output, expected_output);
+
+        let max_widths = calculate_max_widths(&lines);
+        let expected_output = r#"# This is a comment
+
+Model A  [some description]  ->  false
+Model B  12345678            ->  true
+"#;
+
+        let actual_output = generate_aligned_output(&lines, &max_widths);
+
+        assert_eq!(actual_output, expected_output);
     }
 }
