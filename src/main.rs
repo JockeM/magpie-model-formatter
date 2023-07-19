bb52142167d89112062bf6da2f32e90f108d7f2c
Joakim Malmberg <10110847+JockeM@users.noreply.github.com>
2023-07-19 16:36:18 +0200

    Change to parallel execution per file

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use std::io::{BufReader, BufWriter, Read, Write};
 use std::path::{Path, PathBuf};
 use std::time::Instant;
 
+use rayon::prelude::*;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
 
@@ -18,35 +20,27 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let metadata = fs::metadata(&path)?;
 
-    let mut num_files_processed = 0;
     let start_time = Instant::now();
 
     if metadata.is_file() {
         if path.file_name() != Some("model".as_ref()) {
             panic!("Invalid file type. Only model files with the name 'model' are allowed.");
         }
-        process_file(&path, &mut num_files_processed)?;
+        process_file(&path)?;
     } else if metadata.is_dir() {
-        process_directory(&path, &mut num_files_processed)?;
+        process_directory(&path)?;
     } else {
         panic!("Invalid path specified");
     }
 
     let elapsed_time = start_time.elapsed();
 
-    println!(
-        "Processed {} file(s) in {} ms",
-        num_files_processed,
-        elapsed_time.as_millis()
-    );
+    println!("Processed files in {} ms", elapsed_time.as_millis());
 
     Ok(())
 }
 
-fn process_file(
-    file_path: &Path,
-    num_files_processed: &mut usize,
-) -> Result<(), Box<dyn std::error::Error>> {
+fn process_file(file_path: &Path) -> Result<(), Box<dyn std::error::Error>> {
     let file = OpenOptions::new().read(true).open(file_path).unwrap();
     let mut reader = BufReader::new(&file);
     let mut buffer = String::new();
@@ -62,30 +56,24 @@ fn process_file(
     let mut writer = BufWriter::new(&file);
     writer.write_all(string.as_bytes())?;
 
-    *num_files_processed += 1;
     println!("Formatted file: {}", file_path.display());
 
     Ok(())
 }
 
-fn process_directory(
-    dir_path: &Path,
-    num_files_processed: &mut usize,
-) -> Result<(), Box<dyn std::error::Error>> {
+fn process_directory(dir_path: &Path) -> Result<(), Box<dyn std::error::Error>> {
     let entries = fs::read_dir(dir_path)?;
 
-    for entry in entries {
-        let entry = entry?;
-        let path = entry.path();
-
+    entries.into_iter().par_bridge().for_each(|entry| {
+        let path = entry.unwrap().path();
         if path.is_file() {
             if path.file_name() == Some("model".as_ref()) {
-                process_file(&path, num_files_processed)?;
+                process_file(&path).unwrap();
             }
         } else {
-            process_directory(&path, num_files_processed)?;
+            process_directory(&path).unwrap();
         }
-    }
+    });
 
     Ok(())
 }
